c962a7e6d9ad2e79b5b6d954d1b91bfb93c9e60a
Shingo Omura <everpeace@gmail.com>
2016-05-04 17:48:32 +0900

    add multi-jvm test for akka cluster bootstrapping with consutructr-redis fixes #1

--- build.sbt
+++ build.sbt
@@ -2,9 +2,16 @@ lazy val construtrRoot = project
   .copy(id = "constructr-redis-root")
   .in(file("."))
   .enablePlugins(GitVersioning)
-  .aggregate(constructrCoordinationRedis)
+  .aggregate(constructrCoordinationRedis, constructrAkkaTesting)
 
 lazy val constructrCoordinationRedis = project
   .copy(id = "constructr-coordination-redis")
   .in(file("constructr-coordination-redis"))
   .enablePlugins(AutomateHeaderPlugin)
+
+lazy val constructrAkkaTesting = project
+  .copy(id = "constructr-akka-testing")
+  .in(file("constructr-akka-testing"))
+  .enablePlugins(AutomateHeaderPlugin)
+  .configs(MultiJvm)
+  .dependsOn(constructrCoordinationRedis % "test->compile")
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,16 +2,22 @@ import sbt._
 
 object Version {
     final val Scala            = "2.11.8"
-    final val ConstructrAkka   = "0.13.2"
+    final val Constructr       = "0.13.2"
     final val Rediscala        = "1.6.0"
     final val Akka             = "2.4.4"
+    final val AkkaLog4j        = "1.1.3"
+    final val Log4j            = "2.5"
     final val ScalaTest        = "2.2.6"
 }
 
 object Library {
-  val constructrAkka       = "de.heikoseeberger"  %% "constructr-akka"              % Version.ConstructrAkka
-  val rediscala            = "com.github.etaty"         %% "rediscala"                   % Version.Rediscala
-  val akkaTestkit          = "com.typesafe.akka"        %% "akka-testkit"                % Version.Akka
-  val akkaMultiNodeTestkit = "com.typesafe.akka"  %% "akka-multi-node-testkit"      % Version.Akka
-  val scalaTest            = "org.scalatest"      %% "scalatest"                    % Version.ScalaTest
+  val constructrCoordination = "de.heikoseeberger"        %% "constructr-coordination"  % Version.Constructr
+  val constructrAkka         = "de.heikoseeberger"        %% "constructr-akka"          % Version.Constructr
+  val rediscala              = "com.github.etaty"         %% "rediscala"                % Version.Rediscala
+  val akkaCluster            = "com.typesafe.akka"        %% "akka-cluster"             % Version.Akka
+  val akkaTestkit            = "com.typesafe.akka"        %% "akka-testkit"             % Version.Akka
+  val akkaMultiNodeTestkit   = "com.typesafe.akka"        %% "akka-multi-node-testkit"  % Version.Akka
+  val akkaLog4j              = "de.heikoseeberger"        %% "akka-log4j"               % Version.AkkaLog4j
+  val log4jCore              = "org.apache.logging.log4j" %  "log4j-core"               % Version.Log4j
+  val scalaTest              = "org.scalatest"            %% "scalatest"                % Version.ScalaTest
 }
